5cf012f2788d7f8f665fd1ee142678238211e6fc
Mike <mike.drabic@gmail.com>
2011-08-21 13:40:40 -0400

    Test commit to Dev branch

--- strip-debug.pl
+++ strip-debug.pl
@@ -14,41 +14,45 @@
 # 8/12/2011 - Inital Version - v1
 
 
+#Notes & TODO:
+# -use strict vars
+# -Move ARGV stuff to parse_commandLine
+# -Create 
 #-------------------------------------------------------------------------------
-# Extras:
-#*iteractive mode? 
-#*Changeable Tags?
-#-------------------------------------------------------------------------------
+
+
+#Imported Modules 
+use Getopt::Std;
 
 
 
 
-my $debugStart = "\@DEBUG";	     #Remove Code Starting Here
-my $debugFinish = "\@END DEBUG";     #Stop Removing Code Here
-my $cleanSource = @ARGV[1];          #Debug-Free Output file
-my $results = @ARGV[2]; 	     #Listing of Removed Code 
+my $debugTagStart = "\@DEBUG";	     #Remove Code Starting Here
+my $debugTagFinish = "\@END DEBUG";  #Stop Removing Code Here
+my $cleanSource = $ARGV[1];          #Debug-Free Output file
+my $results = $ARGV[2]; 	     #Listing of Removed Code 
 my $debugCode = 0; 	             #State Variable
 my $linesRemoved = 0;                #Number of lines Removed
 my $lineNumber = 0;                  #The current line number 
 
-open SOURCE, '<', @ARGV[0] or die $!;#Source code file
+open SOURCE, '<', $ARGV[0] or die $!;#Source code file
 open CLEAN_SOURCE, '>', $cleanSource or die $!;
 open RESULTS, '>', $results or die "No output file specified\n";
 
 
-print "\nRemoving code between ".$debugStart." and ".$debugFinish."\n";
+print "\nRemoving code between ".$debugTagStart." and ".$debugTagFinish."\n";
 
 
 while (my $line = <SOURCE>){
 	
 	$lineNumber++;
 	
-	if($line =~ /$debugStart/){
+	if($line =~ /$debugTagStart/){
 		$debugCode = 1;	
-		print RESULTS "Removed From: ".@ARGV[0]."\n";	
+		print RESULTS "Removed From: ".$ARGV[0]."\n";	
 		print RESULTS "---------------------------------------\n";
 	}
-	elsif($line =~ /$debugFinish/){
+	elsif($line =~ /$debugTagFinish/){
 		$debugCode = 0;		
 		print RESULTS "\n\n";
 	}
@@ -72,4 +76,63 @@ close RESULTS or die $!;
 
 exit;
 
- 
+
+
+#-------------------------------------------------------------------------------
+
+# name parse_commandLine
+# description setup the environment by determining any command line options
+#             or parameters that were specified. 
+# parmas: none
+# return: none
+sub parse_commandLine_options
+{
+	#Cmd Options
+	my $optDir = 'd';    # root directory to start processing
+	my $optVerbose = 'v'; # verbose execution
+	my $optHelp = 'h';    # help 
+	my $optList = "d:vh";
+
+ 	
+	getopt($optList,\%cmdOptions);
+
+
+	#Parse for 'help' option
+	if( $cmdOptions{$optHelp}  == 1  ){
+		#call/display help()
+		return;
+	}
+
+	#Find if a root prjoect directory specified
+	if( $cmdOptions{$optDir} eq ("" || " ")){
+		#display option value error
+		return
+	} 
+	else {
+		#parse root directory
+	} 
+
+	#Parse for Verbose mode
+	if( $cmdOptions{$optVerbose} == 1 ){
+		#set output mode
+	}
+}
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
